6a231df33c88c1538329a273a1dad914c4a8217d
Amari Duran <jch.duran1@icloud.com>
2022-03-04 18:49:31 -0700

    Initial Commit

---
+++ Brightline/BrightlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrightlineApp.swift
+//  Brightline
+//
+//  Created by Cleopatra on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BrightlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
